3609c854b7828601c1d94130f26d7e59dddaa6bd
Rachit-gupta01 <rachitgupta258@gmail.com>
2026-03-11 21:31:37 +0530

    Initial hospital backend project

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -1,36 +1,66 @@
 
+// const Patient = require("../models/patientModel");
+
+// exports.createPatient = async (req, res) => {
+
+// try {
+
+// const patient = new Patient(req.body);
+
+// await patient.save();
+
+// res.status(201).json(patient);
+
+// } catch (err) {
+
+// res.status(500).json({ error: err.message });
+
+// }
+
+// };
+
+// exports.getPatients = async (req, res) => {
+
+// try {
+
+// const patients = await Patient.find();
+
+// res.json(patients);
+
+// } catch (err) {
+
+// res.status(500).json({ error: err.message });
+
+// }
+
+// };
 const Patient = require("../models/patientModel");
 
+// CREATE
 exports.createPatient = async (req, res) => {
-
-try {
-
-const patient = new Patient(req.body);
-
-await patient.save();
-
-res.status(201).json(patient);
-
-} catch (err) {
-
-res.status(500).json({ error: err.message });
-
-}
-
+ const patient = new Patient(req.body);
+ await patient.save();
+ res.json(patient);
 };
 
+// READ
 exports.getPatients = async (req, res) => {
+ const patients = await Patient.find();
+ res.json(patients);
+};
 
-try {
+// UPDATE
+exports.updatePatient = async (req, res) => {
+ const patient = await Patient.findByIdAndUpdate(
+  req.params.id,
+  req.body,
+  { new: true }
+ );
+ res.json(patient);
+};
 
-const patients = await Patient.find();
-
-res.json(patients);
-
-} catch (err) {
-
-res.status(500).json({ error: err.message });
-
-}
-
+// DELETE
+exports.deletePatient = async (req, res) => {
+ await Patient.findByIdAndDelete(req.params.id);
+ res.json({ message: "Patient deleted" });
 };
--- routes/patientRoutes.js
+++ routes/patientRoutes.js
@@ -1,11 +1,29 @@
 
+// const express = require("express");
+// const router = express.Router();
+
+// const patientController = require("../controllers/patientController");
+
+// router.post("/patients", patientController.createPatient);
+
+// router.get("/patients", patientController.getPatients);
+
+// module.exports = router;
 const express = require("express");
 const router = express.Router();
 
 const patientController = require("../controllers/patientController");
 
+// CREATE
 router.post("/patients", patientController.createPatient);
 
+// READ
 router.get("/patients", patientController.getPatients);
+
+// UPDATE
+router.put("/patients/:id", patientController.updatePatient);
+
+// DELETE
+router.delete("/patients/:id", patientController.deletePatient);
 
 module.exports = router;
