5f959965b75d3854a46e9950d1830b92622a4495
Rachit-gupta01 <rachitgupta258@gmail.com>
2026-03-11 21:37:34 +0530

    Update patientRoutes.js

--- routes/patientRoutes.js
+++ routes/patientRoutes.js
@@ -1,14 +1,5 @@
 
-// const express = require("express");
-// const router = express.Router();
 
-// const patientController = require("../controllers/patientController");
-
-// router.post("/patients", patientController.createPatient);
-
-// router.get("/patients", patientController.getPatients);
-
-// module.exports = router;
 const express = require("express");
 const router = express.Router();
 
