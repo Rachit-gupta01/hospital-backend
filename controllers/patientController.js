dbbff52cdbe202f8f2255c69ddd894f8ecaf8691
Rachit-gupta01 <rachitgupta258@gmail.com>
2026-03-11 21:37:13 +0530

    Update patientController.js

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -1,39 +1,5 @@
 
-// const Patient = require("../models/patientModel");
 
-// exports.createPatient = async (req, res) => {
-
-// try {
-
-// const patient = new Patient(req.body);
-
-// await patient.save();
-
-// res.status(201).json(patient);
-
-// } catch (err) {
-
-// res.status(500).json({ error: err.message });
-
-// }
-
-// };
-
-// exports.getPatients = async (req, res) => {
-
-// try {
-
-// const patients = await Patient.find();
-
-// res.json(patients);
-
-// } catch (err) {
-
-// res.status(500).json({ error: err.message });
-
-// }
-
-// };
 const Patient = require("../models/patientModel");
 
 // CREATE
